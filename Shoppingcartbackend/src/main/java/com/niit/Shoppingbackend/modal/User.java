9f2d454536905c66e0f46fa627383de63323e2ca
bhavya1092 <sampatbhavya@yahoo.in>
2017-01-18 21:04:36 +0530

    Code modified in java files

--- Shoppingcartbackend/src/main/java/com/niit/Shoppingbackend/modal/User.java
+++ Shoppingcartbackend/src/main/java/com/niit/Shoppingbackend/modal/User.java
@@ -1,10 +1,134 @@
-package com.niit.Shoppingbackend.modal;
+package com.niit.shoppingbackend.model;
 
-public class User
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Table (name="user")
+public class User 
 
 {
-	
-}
+
+	    @Id
+		@GeneratedValue(strategy=GenerationType.AUTO)
+		
+		private int userid;
+		
+	    @NotNull(message="name cannot left blank")
+		private String username;
+		
+		@NotNull(message="emailid cannot left blank")
+		private String email;
+		
+		@NotNull(message="phone cannot left blank")
+		private String phno;
+		
+		@NotNull(message="password cannot left blank")
+		private String password;
+		
+	    @Transient
+		@NotNull(message="confirm password cannot left blank")
+		private String cpassword;
+		
+		@NotNull(message="enabled cannot left blank")
+		private String enabled;
+		
+		private String role;
+		public String getRole() 
+		
+		{
+			return role;
+		}
+		
+		public void setRole(String role)
+		
+		{
+			this.role = role;
+		}
+		public String getEnabled()
+		
+		{
+			return enabled;
+		}
+		public void setEnabled(String enabled)
+		
+		{
+			this.enabled = enabled;
+		}
+		public int getUserid()
+		
+		{
+			return userid;
+		}
+		public void setUserid(int userid)
+		
+		{
+			this.userid = userid;
+		}
+		public String getUsername()
+		
+		{
+			return username;
+		}
+		public void setUsername(String username)
+		
+		{
+			this.username = username;
+		}
+		public String getEmail()
+		
+		{
+			return email;
+		}
+		public void setEmail(String email)
+		
+		{
+			this.email = email;
+		}
+		public String getPhno()
+		
+		{
+			return phno;
+		}
+		public void setPhno(String phno)
+		
+		{
+			this.phno = phno;
+		}
+		public String getPassword() 
+		
+		{
+			return password;
+		}
+		public void setPassword(String password)
+		
+		{
+			this.password = password;
+		}
+		public String getCpassword()
+		
+		{
+			return cpassword;
+		}
+		public void setCpassword(String cpassword) 
+		
+		{
+			this.cpassword = cpassword;
+		}
+		
+
+	}
+
+		
+
+
+
 
 
 
