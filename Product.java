1b554fb7f66c4605ad53f3ebb09965f61381665a
bhavya1092 <sampatbhavya@yahoo.in>
2017-02-19 18:07:12 +0530

    commited

---
+++ Product.java
@@ -0,0 +1,125 @@
+package com.niit.shoppingbackend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Entity
+@Table (name="product")
+public class Product 
+
+{
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	
+	private int prod_id;
+	
+	private String prod_name;
+	
+	@Min(1)
+	private int prod_price;
+	
+	@Min(1)
+	private int quantity;
+	
+	@Transient
+	private MultipartFile image;
+	
+	@ManyToOne
+	@JoinColumn(name = "cat_id", insertable = false, updatable = false)
+
+	private Category category;
+	private int cat_id;
+	
+	@ManyToOne
+	@JoinColumn(name = "sup_id", insertable = false, updatable = false)
+	private Supplier supplier;
+
+	public int getCat_id() {
+		return cat_id;
+	}
+	public void setCat_id(int cat_id) {
+		this.cat_id = cat_id;
+	}
+	
+	private int sup_id;
+	
+	public int getSup_id() {
+		return sup_id;
+	}
+	public void setSup_id(int sup_id) {
+		this.sup_id = sup_id;
+	}
+	public Category getCategory() 
+	{
+		return category;
+	}
+	public void setCategory(Category category) 
+	{
+		this.category = category;
+	}
+	
+	public Supplier getSupplier() 
+	{
+		return supplier;
+	}
+	public void setSupplier(Supplier supplier) 
+	{
+		this.supplier = supplier;
+	}
+	
+		public MultipartFile getImage() 
+	
+	{
+		return image;
+	}
+	public void setImage(MultipartFile image)
+	{
+		this.image = image;
+	}
+	public int getProd_id() 
+	{
+		return prod_id;
+	}
+	public void setProd_id(int prod_id) 
+	{
+		this.prod_id = prod_id;
+	}
+	public String getProd_name() 
+	{
+		return prod_name;
+	}
+	public void setProd_name(String prod_name) 
+	{
+		this.prod_name = prod_name;
+	}
+	public int getProd_price() 
+	{
+		return prod_price;
+	}
+	public void setProd_price(int prod_price) 
+	{
+		this.prod_price = prod_price;
+	}
+	public int getQuantity() 
+	{
+		return quantity;
+	}
+	public void setQuantity(int quantity) 
+	
+	{
+		this.quantity = quantity;
+	}
+	
+
+}
